a79cbea226fd472ee4ae836ae7e75b0c937b6c52
alejaramos <iaramosr@gmail.com>
2022-08-30 20:37:54 -0400

    GPT3 implementation

--- writing-proyect/components/InitialTopic.jsx
+++ writing-proyect/components/InitialTopic.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import { useState } from "react";
+
 
 export default function InitialTopic() {
+  const [initialTopic, setInitialTopic] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ initialTopic: initialTopic }),
+    });
+    const data = await response.json();
+    console.log(data);
+    setResult(data.result);
+    setInitialTopic("");
+  }
+
   return (
+    <>
     <div>
       <img
         class="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0"
-      //  src="./public/brilliantIdea.png"
-      src="https://us.123rf.com/450wm/vektoria/vektoria1807/vektoria180700001/108438204-light-bulb-great-idea-bulb-icon-clipart-vector-illustration-.jpg?ver=6"
+        //  src="./public/brilliantIdea.png"
+        src="https://us.123rf.com/450wm/vektoria/vektoria1807/vektoria180700001/108438204-light-bulb-great-idea-bulb-icon-clipart-vector-illustration-.jpg?ver=6"
         alt="Brillian Idea"
       ></img>
       <div class="text-center space-y-2 sm:text-left">
@@ -15,21 +36,30 @@ export default function InitialTopic() {
             <div class="text-xl font-medium text-black">
               Which topic would you like to write about?
             </div>
-            <div class="flex items-center border-b border-purple-600 py-2">
-              <form>
+            <div class="flex items-center ">
+              <form  onSubmit={onSubmit}>
                 <input
-                  class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                  class="appearance-none bg-transparent border-b  py-2 border-purple-600 w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
                   type="text"
                   placeholder="Your topic here"
+                  name="initialTopic"
+                  value={initialTopic}
+                  onChange={(e) => setInitialTopic(e.target.value)}
                 ></input>
+          
+                  <button type="submit" value="Generate topics" class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+          Next
+        </button>
               </form>
             </div>
           </div>
         </div>
-        <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-          Next
-        </button>
       </div>
+
+     
     </div>
+     <div >
+      <p> Here are some suggestions you can use in your title:</p>{result}</div>
+     </>
   );
 }
---
+++ writing-proyect/pages/api/generate.js
@@ -0,0 +1,30 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: generatePrompt(req.body.initialTopic),
+    temperature: 0.8,
+  });
+  res.status(200).json({ result: completion.data.choices[0].text });
+}
+
+function generatePrompt(initialTopic) {
+  const capitalizedInitialTopic =
+  initialTopic[0].toUpperCase() + initialTopic.slice(1).toLowerCase();
+  return `Suggest three titles for an essay.
+
+Topic: Plato
+Title: Plato and politics, Plato and literature, What is the contribution of Plato in Greek literature?
+Topic: Nietzsche
+Title: Nihilism and Nietzsche, Nietzsche and Platonism, Nietzsche 's Philosophy On Morality
+Topic: Hegel and religion
+Title: hegel and religion the rational understanding of reality , religion and the substance of eticity in hegel, christianity as the axis of hegelian philosophy.
+Topic: ${capitalizedInitialTopic}
+Title:`;
+}
